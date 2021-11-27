9b31ad38fd6c62f77ef4dcbd95a9c4d3e55fcd0b
Cesar Josue Muria Gomez <58366298+Bateyss@users.noreply.github.com>
2021-11-27 02:43:21 -0600

    Mejoras V1
    mejoras sobre efectos y formato inicial de pantallas
    agregando mas responsiva

--- src/app/multimedia/multimedia.component.ts
+++ src/app/multimedia/multimedia.component.ts
@@ -28,4 +28,28 @@ export class MultimediaComponent implements OnInit {
     this.animateScrollService.scrollToElement('multimediaBottom', Utils.scrollerDuration)
   }
 
+  videoPlayerStateChange(event:any,id:string){
+    console.log('evento',event);
+    console.log('id',id);
+    let tagTitle = document.getElementById('videoTitle'+id)
+    let tagSubTitle = document.getElementById('videoSubTitle'+id)
+    let tagInfo = document.getElementById('videoInfo'+id)
+    if (event && event.data == YT.PlayerState.PLAYING) {
+      tagTitle?.setAttribute('style','display: none; opacity: 0.1;')
+      tagSubTitle?.setAttribute('style','display: none; opacity: 0.1;')
+      tagInfo?.setAttribute('style','display: none; opacity: 0.1;')
+    }
+    if (event && event.data == YT.PlayerState.PAUSED) {
+      tagTitle?.setAttribute('style','display: block; opacity: 1;')
+      tagSubTitle?.setAttribute('style','display: block; opacity: 1;')
+      tagInfo?.setAttribute('style','display: block; opacity: 1;')
+    }
+    if (event && event.data == YT.PlayerState.ENDED) {
+      tagTitle?.setAttribute('style','display: block; opacity: 1;')
+      tagSubTitle?.setAttribute('style','display: block; opacity: 1;')
+      tagInfo?.setAttribute('style','display: block; opacity: 1;')
+    }
+
+  }
+
 }
